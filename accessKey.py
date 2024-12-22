95f3cbcd62c60ce83b9fdee3fc883f6e01c494b9
Kai Stephens <130561329+KaiStephens@users.noreply.github.com>
2024-12-22 14:16:15 -0500

    Update accessKey.py

--- accessKey.py
+++ accessKey.py
@@ -1,3 +1,3 @@
-pvporcupine_access_key = "0jGjebHIfboyHSbPt289ALfFnk0Ji8wDv7+RJr+xK0sAY8aHSNd8UA=="
-api_key_weather = "d57912ef6c4d5e39679d924ba2253cb8g"
+pvporcupine_access_key = ""
+api_key_weather = ""
 weather_city = ""
